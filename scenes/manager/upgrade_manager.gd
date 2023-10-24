0170d939311c0899f1c23ccd16f54a3e49493f1c
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2023-10-24 18:46:52 +0200

    Feature: Added max_quantity of ability upgrades
    Prevented Abilities from being chosen infinite times. Modified update_manager filter_and_pick_upgrades.

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -23,22 +23,35 @@ func _apply_upgrade(upgrade: AbilityUpgrade):
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
-func _pick_upgrades(quantity: int) -> Array[AbilityUpgrade]:
-	var chosen_upgrades = upgrade_pool.duplicate()
-	chosen_upgrades.shuffle()
-	chosen_upgrades.slice(0, quantity)
+func _filter_and_pick_upgrades(quantity: int) -> Array[AbilityUpgrade]:
+	var filtered_upgrates = upgrade_pool.duplicate()
 	
-	return chosen_upgrades
-
-
-func on_upgrade_selected(upgrade: AbilityUpgrade):
-	_apply_upgrade(upgrade)
+	filtered_upgrates = filtered_upgrates.filter(func (upgrade: AbilityUpgrade): 
+		if !current_upgrades.has(upgrade.id):
+			return true
+		return current_upgrades[upgrade.id]["quantity"] < upgrade.max_quantity
+	)
+	if filtered_upgrates.size() < quantity:
+		quantity = filtered_upgrates.size()
+	elif filtered_upgrates.size() == 0:
+		return filtered_upgrates
+		
+	filtered_upgrates.shuffle()
+	filtered_upgrates.slice(0, quantity)
+	
+	return filtered_upgrates
 
 
 func on_level_up(current_level: int):
-	var chosen_upgrades = _pick_upgrades(2)
+	var chosen_upgrades = _filter_and_pick_upgrades(2)
+	if !chosen_upgrades:
+		return
 	
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
 	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	_apply_upgrade(upgrade)
