324b9cd7f9abb6a2a52572acac6494ebba81b25a
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2023-10-27 15:25:52 +0200

    Feature: Animation to Wizard
    Added a func to velocity_maneger decelerate()
    a parameter is_moving and a func set_is_moving(moving: bool) to Wizard

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,12 +3,19 @@ extends CharacterBody2D
 
 @onready var visuals: Node2D = $Visuals
 @onready var velocity_component: Node = $VelocityComponent
-
+var is_moving: bool = false
 
 func _process(_delta: float) -> void:
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
 	velocity_component.move(self)
 	
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale.x = move_sign
+
+
+func set_is_moving(moving: bool):
+	is_moving = moving
