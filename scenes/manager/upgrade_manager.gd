d313221c5259e55718dc8e3ac09bafcd0f95bcaf
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2023-11-04 15:23:05 +0100

    Texture: UI Theming
    Added a new texture for ui. Change theme.tres and added a new StyleBoxTexture for Panel Control.

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -5,8 +5,18 @@ extends Node
 @export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
+#var upgrade_pool: WeightedTable = WeightedTable.new()
+#var upgrade_axe = preload("res://resources/upgrades/axe.tres")
+#var upgrade_axe_damage = preload("res://resources/upgrades/axe_damage.tres")
+#var upgrade_sword_rate = preload("res://resources/upgrades/sword_rate.tres")
+#var upgrade_sword_damage = preload("res://resources/upgrades/sword_damage.tres")
+
 
 func _ready() -> void:
+#	upgrade_pool.add_item(upgrade_axe, 10)
+#	upgrade_pool.add_item(upgrade_sword_rate, 10)
+#	upgrade_pool.add_item(upgrade_sword_damage, 10)
+	
 	experience_manager.level_up.connect(on_level_up)
 
 
@@ -20,26 +30,34 @@ func _apply_upgrade(upgrade: AbilityUpgrade):
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 	
+#	update_upgrade_pool(upgrade)
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
+#func update_upgrade_pool(chosen_upgrade: AbilityUpgrade):
+#	if chosen_upgrade.id == upgrade_axe.id:
+#		upgrade_pool.add_item(upgrade_axe_damage, 10)
+
+
 func _filter_and_pick_upgrades(quantity: int) -> Array[AbilityUpgrade]:
-	var filtered_upgrates = upgrade_pool.duplicate()
-	
-	filtered_upgrates = filtered_upgrates.filter(func (upgrade: AbilityUpgrade): 
+	var filtered_upgrades = upgrade_pool.duplicate()
+
+	filtered_upgrades = filtered_upgrades.filter(func (upgrade: AbilityUpgrade): 
 		if !current_upgrades.has(upgrade.id):
 			return true
 		return current_upgrades[upgrade.id]["quantity"] < upgrade.max_quantity
 	)
-	if filtered_upgrates.size() < quantity:
-		quantity = filtered_upgrates.size()
-	elif filtered_upgrates.size() == 0:
-		return filtered_upgrates
-		
-	filtered_upgrates.shuffle()
-	filtered_upgrates.slice(0, quantity)
+	if filtered_upgrades.size() < quantity:
+		quantity = filtered_upgrades.size()
+	elif filtered_upgrades.size() == 0:
+		return filtered_upgrades
+
+	filtered_upgrades.shuffle()
+	filtered_upgrades = filtered_upgrades.slice(0, quantity)
+
+	return filtered_upgrades
 	
-	return filtered_upgrates
+#	var chosen_upgrade = upgrade_pool.pick_item()
 
 
 func on_level_up(_current_level: int):
