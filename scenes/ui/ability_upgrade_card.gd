d4043a31fb04f92034cd133fd0d662549b97314b
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2023-11-06 19:29:57 +0100

    Animation: Upgrade Cards
    Added an animation to upgrade cards:
    1. in - card appearance
    2. selected
    3. discard - non selected card fade out faster
    4. hover anim

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -2,12 +2,24 @@ extends PanelContainer
 
 signal selected
 
+
+var disabled = false
+
 @onready var name_label: Label = $VBoxContainer/PanelContainer/NameLabel
 @onready var description_label: Label = $VBoxContainer/DescriptionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hover_animaition_player: AnimationPlayer = $HoverAnimaitionPlayer
 
 
 func _ready() -> void:
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await  get_tree().create_timer(delay).timeout
+	animation_player.play("in")
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
@@ -15,6 +27,31 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 	description_label.text = upgrade.discription
 
 
+func select_card():
+	disabled = true
+	animation_player.play("selected")
+	
+	var cards = get_tree().get_nodes_in_group("upgrade_card")
+	for other_card in cards:
+		if other_card == self:
+			continue
+		
+		other_card.animation_player.play("discard")
+	
+	await animation_player.animation_finished
+	selected.emit()
+
+
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
+	
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func on_mouse_entered():
+	if disabled:
+		return
+	
+	hover_animaition_player.play("hover")
