8259948c9df322aedc5fa150664db2f328864245
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2023-12-06 18:36:50 +0100

    Feature: Different music
    Added a func to change a music scene

--- scenes/autoload/music_player.gd
+++ scenes/autoload/music_player.gd
@@ -2,9 +2,18 @@ extends AudioStreamPlayer
 
 @onready var timer: Timer = $Timer
 
+
 func _ready() -> void:
+	set_music()
 	finished.connect(on_finished)
 	$Timer.timeout.connect(on_timer_timeout)
+
+
+func set_music(audio_path: String = "res://assets/audio/menu_audio.mp3"):
+	var audio = load(audio_path)
+	stream = audio
+	play()
+
 
 
 func on_finished():
@@ -13,3 +22,5 @@ func on_finished():
 
 func on_timer_timeout():
 	play()
+
+
--- scenes/main/LevelManager.gd
+++ scenes/main/LevelManager.gd
@@ -7,6 +7,7 @@ var dungeon_scene = preload("res://scenes/levels/dungeon.tscn")
 func _ready() -> void:
 	var main_hub_instance = main_hub_scene.instantiate()
 	add_child(main_hub_instance)
+	MusicPlayer.set_music("res://assets/audio/village.mp3")
 	GameEvents.dungeon_entered.connect(on_dungeon_entered)
 
 
@@ -17,4 +18,5 @@ func create_dungeoun_instance():
 
 func on_dungeon_entered():
 	$MainHub.queue_free()
+	MusicPlayer.set_music("res://assets/audio/Asking Questions.mp3")
 	call_deferred("create_dungeoun_instance")
